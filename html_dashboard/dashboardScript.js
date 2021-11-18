e0e24015406ccb364624292b50ee48446d9dff77
ishay320 <ishay320@hotmail.com>
2021-11-18 17:46:10 +0200

    cleanup and update

--- html_dashboard/dashboardScript.js
+++
@@ -1,7 +0,0 @@
-// import { data } from "../assets/data/getData"
-
-
-// $(document).ready(function () { alert("hi2") });
-
-// document.getElementById("test").innerHTML = data.getWeatherData();
-// alert("hi")
